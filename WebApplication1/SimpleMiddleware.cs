a29eb7ee22ac7e5a347dcf42b17e316142caba21
Bradley Snyder <brs80@zips.uakron.edu>
2019-07-31 03:43:30 -0400

    initial commit

---
+++ WebApplication1/SimpleMiddleware.cs
@@ -0,0 +1,84 @@
+﻿using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+
+namespace App04
+{
+    public class SimpleMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public SimpleMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            
+            if (context.Request.QueryString.ToString().Contains("About"))
+            {
+                await ReturnIndexPage(context);
+                return;
+            }
+            
+            if (context.Request.QueryString.ToString().Contains("Resume"))
+            {
+                await ReturnPdfPage(context);
+                return;
+            }
+            await _next.Invoke(context);
+        }
+        private static async Task ReturnPdfPage(HttpContext context)
+        {
+            var file = new FileInfo(@"wwwroot\Resume.pdf");
+            byte[] buffer;
+            if (file.Exists)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+                context.Response.ContentType = "application/pdf";
+                buffer = File.ReadAllBytes(file.FullName);
+            }
+            else
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                context.Response.ContentType = "application/pdf";
+                buffer = Encoding.UTF8.GetBytes("Unable to find the requested file");
+            }
+
+            using (var stream = context.Response.Body)
+            {
+                await stream.WriteAsync(buffer, 0, buffer.Length);
+                await stream.FlushAsync();
+            }
+
+            context.Response.ContentLength = buffer.Length;
+        }
+        private static async Task ReturnIndexPage(HttpContext context)
+        {
+            var file = new FileInfo(@"wwwroot\About.html");
+            byte[] buffer;
+            if (file.Exists)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+                context.Response.ContentType = "text/html";
+                buffer = File.ReadAllBytes(file.FullName);
+            }
+            else
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                context.Response.ContentType = "text/plain";
+                buffer = Encoding.UTF8.GetBytes("Unable to find the requested file");
+            }
+
+            using (var stream = context.Response.Body)
+            {
+                await stream.WriteAsync(buffer, 0, buffer.Length);
+                await stream.FlushAsync();
+            }
+
+            context.Response.ContentLength = buffer.Length;
+        }
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App04;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public object Response { get; private set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMiddleware<SimpleMiddleware>();
+            app.UseFileServer();
+            _ = app.UseMvc(routes =>
+              {
+                  routes.MapRoute(
+                      name: "default",
+                      template: "{controller=Home}/{action=Index}");
+              });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("<html><body>");
+                await context.Response.WriteAsync("<button type=\"button\">button</button>");
+                await next();
+                await context.Response.WriteAsync("</body></html>");
+            });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Bradley Snyder");
+            });
+        }
+    }
+}
